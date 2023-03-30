bfc5d1ac5cb51f3a2f89a987f27e99ec15279a78
laura-paullette <laura.paullette@student.moringaschool.com>
2023-03-30 10:40:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+    // TODO: implement string reversing functionality
+  return word;
 }
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
+
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  return word === reversedWord;
+}
+
 
 /* 
   Add your pseudocode here
+  if the reversed string is the same as the input, return true
+  else return false
 */
 
 /*
@@ -15,6 +33,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
